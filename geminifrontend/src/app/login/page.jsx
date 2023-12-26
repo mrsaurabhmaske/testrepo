23a978935c6cb40e9247f0c0d2caeb6d3ed2616d
Saurabh Maske <saurabh.m@inspironlabs.com>
2023-12-26 15:40:46 +0530

    Routing to Dashboard after successful Google Auth - Added

--- geminifrontend/src/app/login/page.jsx
+++ geminifrontend/src/app/login/page.jsx
@@ -1,34 +1,38 @@
 "use client"
-import Link from "next/link"
-import { GoogleLogin } from "@react-oauth/google"
-import { jwtDecode } from "jwt-decode"
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+import { GoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
+const LoginPage = () => {
+  const router = useRouter();
 
+  return (
+    <div className="mainContainer">
+      <form>
+        <h2>Login</h2>
+        <label htmlFor="email">Email</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">Login</button>
+        {/* On success navigate to "/dashboard" */}
+        <GoogleLogin
+          onSuccess={async (credentialResponse) => {
+            const decoded = jwtDecode(credentialResponse.credential);
+            console.log(decoded);
+            router.push("/dashboard");
+          }}
+          onError={() => {
+            console.log("Login Failed");
+          }}
+        />
+        <label className="message">
+          {"Don't have an account?"} <Link href="/signup">Register</Link>
+        </label>
+      </form>
+    </div>
+  );
+};
 
-const LoginPage = ()=>{
-    return (
-        <div className="mainContainer">
-            <form>
-                <h2>Login</h2>
-                <label htmlFor="email">Email</label>
-                <input type="email" name="email" id="email" />
-                <label htmlFor="password">Password</label>
-                <input type="password" name="password" id="password" />
-                <button type="submit">Login</button>
-                <GoogleLogin
-  onSuccess={credentialResponse => {
-    let credentialResponseDecoded = jwtDecode(credentialResponse.credential);
-    console.log(credentialResponseDecoded);
-  }}
-  onError={() => {
-    console.log("Login Failed");
-  }}
-/>
-                <label className="message">{"Don't have an account?"} <Link href="/signup">Register</Link></label>
-            </form>
-            
-        </div>
-    )
-}
-
-export default LoginPage
+export default LoginPage;
