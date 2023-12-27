08a5785b36257eabc45a398abd150d76bcd628f3
Saurabh Maske <saurabh.m@inspironlabs.com>
2023-12-27 12:24:22 +0530

    Added Social Auth

--- geminifrontend/src/app/login/page.jsx
+++ geminifrontend/src/app/login/page.jsx
@@ -1,11 +1,20 @@
 "use client"
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
+// import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from '@react-oauth/google';
+// import { jwtDecode } from "jwt-decode";
 
 const LoginPage = () => {
   const router = useRouter();
+
+const login = useGoogleLogin({
+  onSuccess: tokenResponse => console.log(tokenResponse),
+  onError: error => console.log(error),
+  flow: 'auth-code',
+});
+
+console.log("Login page rerendered! ", Date.now());
 
   return (
     <div className="mainContainer">
@@ -17,20 +26,25 @@ const LoginPage = () => {
         <input type="password" name="password" id="password" />
         <button type="submit">Login</button>
         {/* On success navigate to "/dashboard" */}
-        <GoogleLogin
+        {/* <GoogleLogin
           onSuccess={async (credentialResponse) => {
             const decoded = jwtDecode(credentialResponse.credential);
-            console.log({decoded,credentialResponse});
+            console.log({ans:credentialResponse});
+            console.log();
             router.push("/dashboard");
           }}
           onError={() => {
             console.log("Login Failed");
           }}
-        />
+        /> */}
+
         <label className="message">
           {"Don't have an account?"} <Link href="/signup">Register</Link>
         </label>
       </form>
+      <div className="google-login-container">
+<button className="google-login-btn button" onClick={login}>Google Login🚀</button>
+      </div>
     </div>
   );
 };
